91591320837e2abc0be615c50aab93990e030e17
Mahalakshmi <mmahaece07@gmail.com>
2023-12-19 16:51:19 +0530

    Navbar 2  commit

--- qtify/src/Components/Navbar.jsx
+++ qtify/src/Components/Navbar.jsx
@@ -9,10 +9,7 @@ function Navbar()
 {
     return(
         <div className={styles.container}>
-         <div className={styles.logocontainer}>
          <img src={logo} alt='Qtify-logo' className={styles.logo} />
-         <span className={styles.logotext}>Q tify</span>
-         </div>
          <form className={styles.form}>
          <input type='text' placeholder='Search a song of your choice' className={styles.text} />
          <button className={styles.icon}><SearchIcon /></button>
