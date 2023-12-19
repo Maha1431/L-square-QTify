c2ad5a73d843072dc97807c8f6f537aabe2c61ab
Mahalakshmi <mmahaece07@gmail.com>
2023-12-19 18:38:51 +0530

    Navbar 7 commit

--- qtify/src/Components/Navbar.jsx
+++ qtify/src/Components/Navbar.jsx
@@ -8,14 +8,14 @@ import {ReactComponent as SearchIcon} from "../assets/Search icon.svg"
 function Navbar()
 {
     return(
-        <div className={styles.container}>
+        <nav className={styles.container}>
          <img src={logo} alt='Qtify-logo' className={styles.logo} />
          <form className={styles.form}>
          <input type='text' placeholder='Search a song of your choice' className={styles.text} />
          <button className={styles.icon} type='submit'><SearchIcon /></button>
          </form>
          <button className={styles.button}>Give Feedback</button>
-        </div>
+        </nav>
     )
 }
 export default Navbar;
