5c3103a22054c85f0e8588850c993cbdfcef7b50
Mahalakshmi <mmahaece07@gmail.com>
2023-12-19 20:13:38 +0530

    Hero section 1 commit

--- qtify/src/Components/Herosection.jsx
+++ qtify/src/Components/Herosection.jsx
@@ -8,7 +8,7 @@ function Herosection()
     return(
         <div className={styles.heroContainer}>
             <div >
-               <h1>100 Thousand songs, add-free</h1>
+               <h1>100 Thousand Songs, ad-free</h1>
                <h1>Over thousands podcast episodes </h1>
             </div>
             <div>
