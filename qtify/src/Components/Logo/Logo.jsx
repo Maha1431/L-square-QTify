62c44cbabb0ce8fec1b5d1350066a1ba1976c809
Mahalakshmi <mmahaece07@gmail.com>
2023-12-19 18:33:16 +0530

    Revert "Navbar 6 commit"
    This reverts commit e7f65094cf24e7946edc1a3f6507a4ed7719b1bf.

--- qtify/src/Components/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import logo from "../../assets/logo.png"
-import styles from "./Logo.module.css"
-
-function Logo()
-{
-    return  <img src={logo} alt='Qtify-logo' className={styles.logo} />
-}
-export default Logo;
